ab1c1d4ceedded17a353056ed98b2b6394dd0594
andy-dan <andrzej.daniluk@wp.pl>
2017-09-01 11:45:09 +0200

    new scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a=10,
+    h=15,
+    triangleArea=(a*h/2);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);   
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);  
+var a1 = prompt("Podaj długość podstawy"),
+    h1 = prompt("Podaj wysokość"),
+    triangleArea1=(a1*h1/2);
+alert('Triangle field with base a: ' + a1 + ' and height h: ' + h1 + ' is equal to: ' + triangleArea1);   
+console.log('Triangle field with base a: ' + a1 + ' and height h: ' + h1 + ' is equal to: ' + triangleArea1);
