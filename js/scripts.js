0a6d448502755c55fbf1d854256c6b8ac0811e0f
andy-dan <andrzej.daniluk@wp.pl>
2017-09-02 16:58:43 +0200

    JS6

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+
+function getTriangleArea(a, h) {
+    
+    if (a <= 0 || h <= 0) {console.log("Nieprawidłowe dane")} 
+    else return getTriangleArea = a*h/2;
+   
+}
+
+console.log(getTriangleArea(10,6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(12, 2);
+var triangle3Area = getTriangleArea(54684, 3151);
+
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
